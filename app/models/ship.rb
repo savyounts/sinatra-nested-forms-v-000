30611d243a517e65555a5868f74bcfe1c146ce67
Sav Younts <savyounts@gmail.com>
2018-09-04 22:13:20 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+  attr_reader :name, :type, :booty
+  @@all = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight =  params[:type]
+    @height = params[:booty]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear 
+    @all.clear
+  end
 end
