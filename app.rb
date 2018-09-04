91f83ebfb5935ce8a2f2986cd70686db4eb0c1e9
Sav Younts <savyounts@gmail.com>
2018-09-04 22:51:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ module FormsLab
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
 
-      pirate[ships].each do |ship|
+      params[pirate][ships].each do |ship|
         Ship.new(ship)
       end
 
